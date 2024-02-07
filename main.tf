4bbfe476578d0b7c759c93e572a7890d779a023e
Julian <eng@finisterra.io>
2024-02-06 22:02:25 -0500

    Update AWS provider versions and remove unused code

--- main.tf
+++ main.tf
@@ -1,10 +1,4 @@
-data "aws_region" "current" {}
-
-data "aws_canonical_user_id" "this" {}
-
 data "aws_caller_identity" "current" {}
-
-data "aws_partition" "current" {}
 locals {
   create_bucket = var.create_bucket
 
@@ -12,7 +6,6 @@ locals {
 
   grants               = try(jsondecode(var.grant), var.grant)
   cors_rules           = try(jsondecode(var.cors_rule), var.cors_rule)
-  intelligent_tiering  = try(jsondecode(var.intelligent_tiering), var.intelligent_tiering)
   metric_configuration = try(jsondecode(var.metric_configuration), var.metric_configuration)
 }
 
@@ -521,36 +514,6 @@ resource "aws_s3_bucket_policy" "this" {
   ]
 }
 
-# AWS Load Balancer access log delivery policy
-locals {
-  # List of AWS regions where permissions should be granted to the specified Elastic Load Balancing account ID ( https://docs.aws.amazon.com/elasticloadbalancing/latest/application/enable-access-logging.html#attach-bucket-policy )
-  elb_service_accounts = {
-    us-east-1      = "127311923021"
-    us-east-2      = "033677994240"
-    us-west-1      = "027434742980"
-    us-west-2      = "797873946194"
-    af-south-1     = "098369216593"
-    ap-east-1      = "754344448648"
-    ap-south-1     = "718504428378"
-    ap-northeast-1 = "582318560864"
-    ap-northeast-2 = "600734575887"
-    ap-northeast-3 = "383597477331"
-    ap-southeast-1 = "114774131450"
-    ap-southeast-2 = "783225319266"
-    ap-southeast-3 = "589379963580"
-    ca-central-1   = "985666609251"
-    eu-central-1   = "054676820928"
-    eu-west-1      = "156460612806"
-    eu-west-2      = "652711504416"
-    eu-west-3      = "009996457667"
-    eu-south-1     = "635631232127"
-    eu-north-1     = "897822967062"
-    me-south-1     = "076674570225"
-    sa-east-1      = "507241528517"
-    us-gov-west-1  = "048591011584"
-    us-gov-east-1  = "190560391635"
-  }
-}
 
 resource "aws_s3_bucket_public_access_block" "this" {
   count = local.create_bucket && var.attach_public_policy ? 1 : 0
